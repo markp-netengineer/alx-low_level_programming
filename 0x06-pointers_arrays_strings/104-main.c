ff7cd50a47866f507759f7dc5697630f40adb37d
markp-netengineer <markphillipkirabo@gmai.com>
2023-03-03 16:04:53 -0800

    r

--- 0x06-pointers_arrays_strings/104-main.c
+++ 0x06-pointers_arrays_strings/104-main.c
@@ -1,18 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 /**
-  main - check the code
+* main - Entry point
 *
-* Return: Always 0.
+* Return: Always 0 (Success)
 */
 int main(void)
 {
 char buffer[] =
-"This is a string!\0And this is the rest of the #buffer :)\1\2\3\4\5\6\
-7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x5
-6#pointersarefun#infernumisfun\n";
+"This is a string!\0And this is the rest of the #buffer :)\1\2\3\4\5\6"
+"7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x5"
+"6#pointersarefun#infernumisfun\n";
 printf("%s\n", buffer);
 printf("---------------------------------\n");
 print_buffer(buffer, sizeof(buffer));
+
 return (0);
 }
+
